36fcdd3694637b60eb017c4bc15510c76acb6981
Shadugr <shadircomm@gmail.com>
2023-08-16 00:37:11 +0300

    Initial

---
+++ UniversityApplicantParcer/Program.cs
@@ -0,0 +1,22 @@
+﻿using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var url = @"https://vstup.edbo.gov.ua/offer/1218942/";
+        var browser = new EdgeDriver(Environment.CurrentDirectory);
+        browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        browser.Navigate().GoToUrl(url);
+
+        var web = browser.FindElement(By.TagName("html"));
+        var htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(web.GetAttribute("innerHTML"));
+        var node = htmlDoc.DocumentNode
+            .Descendants()
+            .Where(node1 => node1.HasClass("offer-request")).Skip(1).ToList();
+        Console.WriteLine(node[0].InnerText);
+    }
+}
